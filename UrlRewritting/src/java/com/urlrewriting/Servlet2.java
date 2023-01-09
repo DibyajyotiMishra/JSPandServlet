49ae87f9efa22be4d01f56597894ff367bbc2271
Dibyajyoti Mishra <dibyajyotimishra14@gmail.com>
2023-01-09 16:24:20 +0530

    feat: learn url rewriting (fetching and setting data from url patterns)

---
+++ UrlRewritting/src/java/com/urlrewriting/Servlet2.java
@@ -0,0 +1,95 @@
+package com.urlrewriting;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author dibyajyotimishra
+ */
+public class Servlet2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servlet2</title>");
+            out.println("<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">");
+            out.println("<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>");
+            out.println("<link href=\"https://fonts.googleapis.com/css2?family=Rowdies:wght@300&display=swap\" rel=\"stylesheet\">");
+            out.println("<style>");
+            out.println("h1, h3 {");
+            out.println("font-family: 'Rowdies', cursive;");
+            out.println("}");
+            out.println("</style>");
+            out.println("</head>");
+            out.println("<body>");
+            
+            out.println("<h1>Welcome to servlet 2</h1>");
+            
+            // fetch the name
+            String name = request.getParameter("user");
+            out.println("<h3 style='color: #66AD47'>Current User: "+ name + "</h3>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
